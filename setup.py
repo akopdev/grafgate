2f605006788cb80aa5a3ff94f6f5df1367684ab7
Akop Kesheshyan <hello@akop.dev>
2022-10-31 00:01:16 +0200

    Init project structure and base api

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Basic info
+    name='grafgate',
+    version=open('VERSION').read().strip(),
+    author='Akop Kesheshyan',
+    author_email='hello@akop.dev',
+    url='https://github.com/akopkesheshyan/grafgate',
+    license='MIT',
+    description='Toolkit for building Grafana data source backend',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp',
+        'pydantic'
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'safety',
+            'pydocstyle',
+            'piprot',
+            'pytest',
+            'pytest-cov',
+            'pytest-asyncio',
+        ],
+    },
+    zip_safe=False,
+    platforms='any',
+)
